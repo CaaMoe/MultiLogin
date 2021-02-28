ac40826e9744e7cf4c6a0af48d5fb0184e97a8c8
ksqeib <ksqeib@ksnb.fun>
2021-02-28 20:40:36 +0800

    删除不必要的代码

--- bungee/src/main/java/moe/caa/multilogin/bungee/proxy/MultiLoginEncryptionResponse.java
+++ bungee/src/main/java/moe/caa/multilogin/bungee/proxy/MultiLoginEncryptionResponse.java
@@ -1,7 +1,6 @@
 package moe.caa.multilogin.bungee.proxy;
 
 import com.google.common.base.Preconditions;
-import io.netty.buffer.ByteBuf;
 import moe.caa.multilogin.bungee.impl.MultiLoginBungee;
 import moe.caa.multilogin.bungee.task.AuthTask;
 import moe.caa.multilogin.core.MultiCore;
@@ -16,7 +15,6 @@ import net.md_5.bungee.netty.ChannelWrapper;
 import net.md_5.bungee.netty.cipher.CipherDecoder;
 import net.md_5.bungee.netty.cipher.CipherEncoder;
 import net.md_5.bungee.protocol.AbstractPacketHandler;
-import net.md_5.bungee.protocol.ProtocolConstants;
 import net.md_5.bungee.protocol.packet.EncryptionRequest;
 import net.md_5.bungee.protocol.packet.EncryptionResponse;
 
@@ -27,14 +25,12 @@ import java.math.BigInteger;
 import java.net.InetSocketAddress;
 import java.net.URLEncoder;
 import java.security.MessageDigest;
-import java.util.Arrays;
 
 public class MultiLoginEncryptionResponse extends EncryptionResponse {
     private static Class<?> INITIAL_HANDLE_CLASS_STATE_CLASS;
     private static Field THIS_STATE;
     private static Field REQUEST;
     private static Field CHANNEL_WRAPPER;
-
 
     public static void init() throws ClassNotFoundException {
         Class<InitialHandler> INITIAL_HANDLE_CLASS = InitialHandler.class;
@@ -44,25 +40,12 @@ public class MultiLoginEncryptionResponse extends EncryptionResponse {
         CHANNEL_WRAPPER = ReflectUtil.getField(INITIAL_HANDLE_CLASS, ChannelWrapper.class);
     }
 
-    private byte[] sharedSecret;
-    private byte[] verifyToken;
-
-    public void read(ByteBuf buf, ProtocolConstants.Direction direction, int protocolVersion) {
-        this.sharedSecret = readArray(buf, 128);
-        this.verifyToken = readArray(buf, 128);
-    }
-
-    public void write(ByteBuf buf, ProtocolConstants.Direction direction, int protocolVersion) {
-        writeArray(this.sharedSecret, buf);
-        writeArray(this.verifyToken, buf);
-    }
-
-    // handler.handle(this);
     public void handle(AbstractPacketHandler handler) throws Exception {
         if (!(handler instanceof InitialHandler)) {
             handler.handle(this);
             return;
         }
+        InitialHandler initialHandler = (InitialHandler) handler;
         try {
             EncryptionRequest request = (EncryptionRequest) REQUEST.get(handler);
             ChannelWrapper ch = (ChannelWrapper) CHANNEL_WRAPPER.get(handler);
@@ -76,7 +59,7 @@ public class MultiLoginEncryptionResponse extends EncryptionResponse {
             ch.addBefore("frame-decoder", "decrypt", new CipherDecoder(decrypt));
             BungeeCipher encrypt = EncryptionUtil.getCipher(true, sharedKey);
             ch.addBefore("frame-prepender", "encrypt", new CipherEncoder(encrypt));
-            String encName = URLEncoder.encode(((InitialHandler) handler).getName(), "UTF-8");
+            String encName = URLEncoder.encode(initialHandler.getName(), "UTF-8");
             MessageDigest sha = MessageDigest.getInstance("SHA-1");
             byte[][] var7 = new byte[][]{request.getServerId().getBytes("ISO_8859_1"), sharedKey.getEncoded(), EncryptionUtil.keys.getPublic().getEncoded()};
 
@@ -84,72 +67,14 @@ public class MultiLoginEncryptionResponse extends EncryptionResponse {
                 sha.update(bit);
             }
             String encodedHash = URLEncoder.encode((new BigInteger(sha.digest())).toString(16), "UTF-8");
-            String preventProxy = BungeeCord.getInstance().config.isPreventProxyConnections() && ((InitialHandler) handler).getSocketAddress() instanceof InetSocketAddress ? "&ip=" + URLEncoder.encode(((InitialHandler) handler).getAddress().getAddress().getHostAddress(), "UTF-8") : "";
+            String preventProxy = BungeeCord.getInstance().config.isPreventProxyConnections() && initialHandler.getSocketAddress() instanceof InetSocketAddress ? "&ip=" + URLEncoder.encode(initialHandler.getAddress().getAddress().getHostAddress(), "UTF-8") : "";
             String arg = String.format("hasJoined?username=%s&serverId=%s%s", encName, encodedHash, preventProxy);
 
-            BungeeCord.getInstance().getScheduler().runAsync(MultiLoginBungee.INSTANCE, new AuthTask((InitialHandler) handler, arg));
+            BungeeCord.getInstance().getScheduler().runAsync(MultiLoginBungee.INSTANCE, new AuthTask(initialHandler, arg));
         } catch (Exception e) {
             e.printStackTrace();
-            ((InitialHandler) handler).disconnect(new TextComponent(PluginData.configurationConfig.getString("msgNoAdopt")));
+            initialHandler.disconnect(new TextComponent(PluginData.configurationConfig.getString("msgNoAdopt")));
             MultiCore.getPlugin().getPluginLogger().severe("处理用户数据时出现异常");
         }
-
-
-    }
-
-    public byte[] getSharedSecret() {
-        return this.sharedSecret;
-    }
-
-    public byte[] getVerifyToken() {
-        return this.verifyToken;
-    }
-
-    public void setSharedSecret(byte[] sharedSecret) {
-        this.sharedSecret = sharedSecret;
-    }
-
-    public void setVerifyToken(byte[] verifyToken) {
-        this.verifyToken = verifyToken;
-    }
-
-    public String toString() {
-        return "EncryptionResponse(sharedSecret=" + Arrays.toString(this.getSharedSecret()) + ", verifyToken=" + Arrays.toString(this.getVerifyToken()) + ")";
-    }
-
-    public MultiLoginEncryptionResponse() {
-    }
-
-    public MultiLoginEncryptionResponse(byte[] sharedSecret, byte[] verifyToken) {
-        this.sharedSecret = sharedSecret;
-        this.verifyToken = verifyToken;
-    }
-
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        } else if (!(o instanceof EncryptionResponse)) {
-            return false;
-        } else {
-            MultiLoginEncryptionResponse other = (MultiLoginEncryptionResponse) o;
-            if (!other.canEqual(this)) {
-                return false;
-            } else if (!Arrays.equals(this.getSharedSecret(), other.getSharedSecret())) {
-                return false;
-            } else {
-                return Arrays.equals(this.getVerifyToken(), other.getVerifyToken());
-            }
-        }
-    }
-
-    protected boolean canEqual(Object other) {
-        return other instanceof MultiLoginEncryptionResponse;
-    }
-
-    public int hashCode() {
-        int result = 1;
-        result = result * 59 + Arrays.hashCode(this.getSharedSecret());
-        result = result * 59 + Arrays.hashCode(this.getVerifyToken());
-        return result;
     }
 }
