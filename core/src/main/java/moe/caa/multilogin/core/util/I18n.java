2c8a94456bec12cc6c7e46d2830d63489b8661e1
CaaMoe <Aeva_D@hotmail.com>
2021-03-13 12:39:53 +0800

    修改语言文件

--- core/src/main/java/moe/caa/multilogin/core/util/I18n.java
+++ core/src/main/java/moe/caa/multilogin/core/util/I18n.java
@@ -1,9 +1,20 @@
+/*
+ * Copyleft (c) 2021 ksqeib,CaaMoe. All rights reserved.
+ * @author  ksqeib <ksqeib@dalao.ink> <https://github.com/ksqeib445>
+ * @author  CaaMoe <miaolio@qq.com> <https://github.com/CaaMoe>
+ * @github  https://github.com/CaaMoe/MultiLogin
+ *
+ * moe.caa.multilogin.core.util.I18n
+ *
+ * Use of this source code is governed by the GPLv3 license that can be found via the following link.
+ * https://github.com/CaaMoe/MultiLogin/blob/master/LICENSE
+ */
+
 package moe.caa.multilogin.core.util;
 
 import moe.caa.multilogin.core.MultiCore;
 
 import java.text.MessageFormat;
-import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class I18n {
@@ -11,7 +22,7 @@ public class I18n {
 
     public static void initService() {
         rb = ResourceBundle.getBundle("lang/multilogin");
-        MultiCore.info(getTransString("load_language", Locale.getDefault()));
+        MultiCore.info(getTransString("load_language"));
     }
 
     public static String getTransString(String key, Object... args) {
