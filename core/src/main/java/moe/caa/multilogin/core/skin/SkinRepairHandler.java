0b7435abb4dcf96d294aa1eced400cd1bdd979b9
CaaMoe <70059785+CaaMoe@users.noreply.github.com>
2021-03-21 23:59:25 +0800

    Update SkinRepairHandler.java

--- core/src/main/java/moe/caa/multilogin/core/skin/SkinRepairHandler.java
+++ core/src/main/java/moe/caa/multilogin/core/skin/SkinRepairHandler.java
@@ -28,19 +28,23 @@ public class SkinRepairHandler {
         String skin = getSkinUrl(new String(onlineProperty.getDecoderValue()));
         String cacSkin = getSkinUrl(new String(Optional.of(property).map(UserProperty::getProperty).map(UserProperty.Property::getDecoderValue).orElse(new byte[0])));
 
+// 判断当前皮肤url是否为空 或 判断当前皮肤url是官方
         if(PluginData.isEmpty(skin) || skin.contains("minecraft.net")) {
             property.setRepair_property(onlineProperty);
             property.setProperty(onlineProperty);
             return false;
         }
 
+// 判断是否缓存过
         if(skin.equalsIgnoreCase(cacSkin))
             return false;
 
+// 修复
         JsonObject jo = new JsonObject();
         jo.addProperty("url", skin);
         String response = HttpGetter.httpPost("https://api.mineskin.org/generate/url", jo.toString(), 1);
 
+// 写入数据
         JsonObject value = new JsonParser().parse(response).getAsJsonObject();
         if(value.has("data")){
             JsonObject data = value.get("data").getAsJsonObject().get("texture").getAsJsonObject();
@@ -61,12 +65,17 @@ public class SkinRepairHandler {
     }
 
     public static UserProperty repairThirdPartySkin(UUID onlineUuid, String value, String signature) throws Exception {
+
+// 判断是否启用该功能
         if(!PluginData.isOpenSkinRepair()) return new UserProperty(onlineUuid, null, new UserProperty.Property(value, signature));
         boolean newUserEntry;
         UserProperty userProperty = SQLHandler.getUserPropertyByOnlineUuid(onlineUuid);
         newUserEntry = userProperty == null;
+
+// 新建皮肤档案
         userProperty = userProperty == null ? new UserProperty(onlineUuid, new UserProperty.Property(), new UserProperty.Property()) : userProperty;
 
+// 修复皮肤
         repairThirdPartySkin(userProperty, new UserProperty.Property(value, signature));
         userProperty.setProperty(new UserProperty.Property(value, signature));
         if(newUserEntry){
