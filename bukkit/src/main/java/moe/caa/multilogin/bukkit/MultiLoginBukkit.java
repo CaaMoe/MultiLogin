84957a4db5519961a2790fe4f0ae0cb8ef9a7962
CaaMoe <Aeva_D@hotmail.com>
2021-07-17 21:37:16 +0800

    完善 Bukkit 部分

--- bukkit/src/main/java/moe/caa/multilogin/bukkit/MultiLoginBukkit.java
+++ bukkit/src/main/java/moe/caa/multilogin/bukkit/MultiLoginBukkit.java
@@ -1,16 +1,25 @@
 package moe.caa.multilogin.bukkit;
 
 import com.google.gson.Gson;
+import com.mojang.authlib.minecraft.HttpMinecraftSessionService;
+import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.authlib.yggdrasil.response.HasJoinedMinecraftServerResponse;
+import moe.caa.multilogin.bukkit.auth.MultiLoginYggdrasilMinecraftSessionService;
 import moe.caa.multilogin.core.data.User;
 import moe.caa.multilogin.core.impl.IPlugin;
 import moe.caa.multilogin.core.impl.ISchedule;
 import moe.caa.multilogin.core.impl.ISender;
+import moe.caa.multilogin.core.language.LanguageKeys;
+import moe.caa.multilogin.core.logger.LoggerLevel;
+import moe.caa.multilogin.core.logger.MultiLogger;
 import moe.caa.multilogin.core.main.MultiCore;
+import moe.caa.multilogin.core.util.ReflectUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -21,10 +30,31 @@ public class MultiLoginBukkit extends JavaPlugin implements IPlugin {
     public static final Map<UUID, Long> LOGIN_CACHE = new Hashtable<>();
     public static final Map<UUID, User> USER_CACHE = new Hashtable<>();
 
+    private void initCoreService() throws Exception {
+        Class<?> craftServerClass = getServer().getClass();
+        Method craftServerGetHandle = craftServerClass.getDeclaredMethod("getHandle");
+        Class<?> dedicatedPlayerListClass = craftServerGetHandle.getReturnType();
+        Method dedicatedPlayerListGetHandler = dedicatedPlayerListClass.getDeclaredMethod("getServer");
+        Class<?> minecraftServerClass = dedicatedPlayerListGetHandler.getReturnType();
+
+        Field field = ReflectUtil.getField(minecraftServerClass, MinecraftSessionService.class, true);
+        Object obj = dedicatedPlayerListGetHandler.invoke(craftServerGetHandle.invoke(getServer()));
+
+        HttpMinecraftSessionService vanServer = (HttpMinecraftSessionService) field.get(obj);
+        MultiLoginYggdrasilMinecraftSessionService mlymss = new MultiLoginYggdrasilMinecraftSessionService(vanServer.getAuthenticationService());
+        mlymss.setVanService(vanServer);
+        field.set(obj, mlymss);
+    }
+
     @Override
     public void onEnable() {
         schedule = new BukkitSchedule(this);
-        System.out.println(MultiCore.init(this));
+        try {
+            initCoreService();
+        } catch (Exception e) {
+            MultiLogger.log(LoggerLevel.ERROR, e);
+            MultiLogger.log(LoggerLevel.ERROR, LanguageKeys.ERROR_REDIRECT_MODIFY.getMessage());
+        }
     }
 
     @Override
---
+++ bukkit/src/main/java/moe/caa/multilogin/bukkit/listener/BukkitListener.java
@@ -0,0 +1,55 @@
+package moe.caa.multilogin.bukkit.listener;
+
+import moe.caa.multilogin.bukkit.MultiLoginBukkit;
+import moe.caa.multilogin.core.data.User;
+import moe.caa.multilogin.core.data.database.handler.UserDataHandler;
+import moe.caa.multilogin.core.language.LanguageKeys;
+import moe.caa.multilogin.core.main.MultiCore;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.UUID;
+
+public class BukkitListener implements Listener {
+    public static final Map<Thread, String> AUTH_CACHE = new Hashtable<>();
+
+    @EventHandler
+    private void onLogin(AsyncPlayerPreLoginEvent event) {
+        String msg = AUTH_CACHE.remove(Thread.currentThread());
+        if (msg != null) {
+            event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_OTHER);
+            event.setKickMessage(msg);
+            return;
+        }
+        if (MultiLoginBukkit.LOGIN_CACHE.remove(event.getUniqueId()) == null) {
+            event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_OTHER);
+            event.setKickMessage(LanguageKeys.VERIFICATION_NO_ADAPTER.getMessage());
+        }
+
+        try {
+            for (User user : UserDataHandler.getUserEntryByRedirectUuid(event.getUniqueId())) {
+                if (user.currentName.equals(event.getName())){
+                    MultiLoginBukkit.USER_CACHE.put(event.getUniqueId(), user);
+                    return;
+                }
+            }
+        } catch (Exception ignored) {
+        }
+        event.setLoginResult(AsyncPlayerPreLoginEvent.Result.KICK_OTHER);
+        event.setKickMessage(LanguageKeys.VERIFICATION_NO_ADAPTER.getMessage());
+    }
+
+    @EventHandler
+    private void onQuit(PlayerQuitEvent event) {
+        UUID uuid = event.getPlayer().getUniqueId();
+        MultiCore.plugin.getSchedule().runTask(() -> {
+            if (MultiCore.plugin.getPlayer(uuid) == null) {
+                MultiLoginBukkit.USER_CACHE.remove(uuid);
+            }
+        });
+    }
+}
