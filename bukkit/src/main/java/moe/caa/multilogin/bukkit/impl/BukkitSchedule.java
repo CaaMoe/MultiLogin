84d5ba811b5e366cf397fab2ed43ef15202eadbd
CaaMoe <Aeva_D@hotmail.com>
2021-07-19 16:28:00 +0800

    TICK 变 毫秒

--- bukkit/src/main/java/moe/caa/multilogin/bukkit/impl/BukkitSchedule.java
+++ bukkit/src/main/java/moe/caa/multilogin/bukkit/impl/BukkitSchedule.java
@@ -17,17 +17,17 @@ public class BukkitSchedule implements ISchedule {
 
     @Override
     public void runTaskAsync(Runnable run, long delay) {
-        PLUGIN.getServer().getScheduler().runTaskLaterAsynchronously(PLUGIN, run, delay);
+        PLUGIN.getServer().getScheduler().runTaskLaterAsynchronously(PLUGIN, run, delay / 50);
     }
 
     @Override
     public void runTaskAsyncTimer(Runnable run, long delay, long per) {
-        PLUGIN.getServer().getScheduler().runTaskTimerAsynchronously(PLUGIN, run, delay, per);
+        PLUGIN.getServer().getScheduler().runTaskTimerAsynchronously(PLUGIN, run, delay / 50, per / 50);
     }
 
     @Override
     public void runTask(Runnable run, long delay) {
-        PLUGIN.getServer().getScheduler().runTaskLater(PLUGIN, run, delay);
+        PLUGIN.getServer().getScheduler().runTaskLater(PLUGIN, run, delay / 50);
     }
 
     @Override
