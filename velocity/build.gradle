36c0d8d1f7c00fc789fe2e287a3fafa5e9a28c9f
BecodReyes <35525491+BecodReyes@users.noreply.github.com>
2021-10-08 13:29:53 +0000

    Repairing velocity compilation failed

--- velocity/build.gradle
+++ velocity/build.gradle
@@ -3,8 +3,9 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
 }
 
 repositories {
