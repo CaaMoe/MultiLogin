16040b617e1bfceffbbf23cb8747c82bfc37f9a3
CaaMoe <Aeva_D@hotmail.com>
2021-10-10 13:02:28 +0800

    修改 gradle 配置文件，更改依赖识别方式

--- core/loader/src/main/java/moe/caa/multilogin/core/loader/libraries/Library.java
+++ core/loader/src/main/java/moe/caa/multilogin/core/loader/libraries/Library.java
@@ -1,6 +1,9 @@
 package moe.caa.multilogin.core.loader.libraries;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.io.FileInputStream;
 import java.util.*;
@@ -8,41 +11,75 @@ import java.util.*;
 /**
  * 代表一个依赖对象
  */
+@Getter(value = AccessLevel.PROTECTED)
+@ToString
+@Builder(access = AccessLevel.PRIVATE)
 public class Library {
 
-    @Getter
+    @Getter(value = AccessLevel.PROTECTED)
     private static final List<Library> LIBRARIES;
 
     static {
-        try {
-            Properties properties = new Properties();
-            //properties.load(Library.class.getResourceAsStream("libraries.properties"));
-            properties.load(new FileInputStream("D:\\Desktop\\libraries.properties"));
-
-            List<Library> libraries = new ArrayList<>();
-            for (Map.Entry<Object, Object> entry : properties.entrySet()) {
-                String[] args = entry.getValue().toString().split("\\s+");
-                libraries.add(new Library(args[0], args[1], args[2], null, null));
-            }
-            LIBRARIES = Arrays.asList(libraries.toArray(new Library[0]));
-        } catch (Throwable t){
-            throw new RuntimeException("AN ERROR OCCURRED IN READING FILE libraries.properties.", t);
-        }
+        LIBRARIES = Arrays.asList(
+                Library.builder()
+                        .group("org.slf4j")
+                        .name("slf4j-api")
+                        .version("1.7.32")
+                        .mainClass("org.slf4j.LoggerFactory")
+                        .build(),
+                Library.builder()
+                        .group("org.apache.logging.log4j")
+                        .name("log4j-core")
+                        .version("2.14.1")
+                        .mainClass("org.apache.logging.log4j.core.LoggerContext")
+                        .build(),
+                Library.builder()
+                        .group("org.apache.logging.log4j")
+                        .name("log4j-api")
+                        .version("2.14.1")
+                        .mainClass("org.apache.logging.log4j.Level")
+                        .build(),
+                Library.builder()
+                        .group("com.zaxxer")
+                        .name("HikariCP")
+                        .version("4.0.3")
+                        .mainClass("com.zaxxer.hikari.HikariDataSource")
+                        .forwardPackage("com.zaxxer.hikari")
+                        .relocate("moe.caa.multilogin.lib.com.zaxxer.hikari")
+                        .build(),
+                Library.builder()
+                        .group("mysql")
+                        .name("mysql-connector-java")
+                        .version("8.0.25")
+                        .mainClass("com.mysql.cj.jdbc.MysqlDataSource")
+                        .forwardPackage("com.mysql")
+                        .relocate("moe.caa.multilogin.lib.com.mysql")
+                        .build(),
+                Library.builder()
+                        .group("com.h2database")
+                        .name("h2")
+                        .version("1.4.200")
+                        .mainClass("org.h2.jdbcx.JdbcConnectionPool")
+                        .build(),
+                Library.builder()
+                        .group("com.google.code.gson")
+                        .name("gson")
+                        .version("2.8.8")
+                        .mainClass("com.google.gson.Gson")
+                        .forwardPackage("com.google.gson")
+                        .relocate("moe.caa.multilogin.lib.com.google.gson")
+                        .build(),
+                Library.builder()
+                        .group("org.yaml")
+                        .name("snakeyaml")
+                        .version("1.29")
+                        .mainClass("org.yaml.snakeyaml.DumperOptions")
+                        .forwardPackage("org.yaml.snakeyaml")
+                        .relocate("moe.caa.multilogin.lib.org.yaml.snakeyaml")
+                        .build()
+        );
     }
 
-    private Library(String group, String name, String version, String rc, String abp) {
-        this.group = group;
-        this.name = name;
-        this.version = version;
-        this.rc = rc;
-        this.abp = abp;
-    }
-
-    public static void main(String[] args) {
-        for (Library library : LIBRARIES) {
-            System.out.println(library.generateDownloadUrl());
-        }
-    }
 
     /**
      * 组
@@ -62,12 +99,17 @@ public class Library {
     /**
      * 代表类名称，不含重定向
      */
-    private final String rc;
+    private final String mainClass;
 
     /**
-     * 重定向前追加的包名
+     * 需要重定向的包名
      */
-    private final String abp;
+    private final String forwardPackage;
+
+    /**
+     * 重定向后的包名
+     */
+    private final String relocate;
 
     /**
      * 生成 Jar 包名称
@@ -77,6 +119,35 @@ public class Library {
         return name + "-" + version + ".jar";
     }
 
+    /**
+     * 需要重定向
+     * @return 需要重定向
+     */
+    public boolean needRelocate(){
+        return relocate != null;
+    }
+
+    /**
+     * 测试重定向后的Class是否已被成功加载
+     *
+     * @param loader 加载的类加载器
+     * @return 是否已被成功加载
+     */
+    public boolean isLoaded(ClassLoader loader){
+        String className = needRelocate() ? mainClass.replace(forwardPackage, relocate) : mainClass;
+        System.out.println(className);
+        try {
+            Class.forName(className, true, loader);
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    /**
+     * 生成下载链接
+     * @return 下载链接
+     */
     public String generateDownloadUrl(){
         // 例子 方便生成URL   https://repo1.maven.org/maven2/com/zaxxer/HikariCP/4.0.2/HikariCP-4.0.2.jar
         StringBuilder sb = new StringBuilder("https://repo1.maven.org/maven2/");
