0bbf360f406d63e9d3633e6cbd35041a89b80886
CaaMoe <Aeva_D@hotmail.com>
2021-10-23 19:07:00 +0800

    移除数据库列名高级设置选项

--- core/src/main/java/moe/caa/multilogin/core/data/config/AdvancedSetting.java
+++ core/src/main/java/moe/caa/multilogin/core/data/config/AdvancedSetting.java
@@ -23,21 +23,19 @@ import java.util.Properties;
 public class AdvancedSetting {
     private transient final Properties properties = new Properties();
     private String database_user_data_table_name = "{prefix}{_}user_data";
-    private String database_user_data_table_field_online_uuid = "online_uuid";
-    private String database_user_data_table_field_current_name = "current_name";
-    private String database_user_data_table_field_redirect_uuid = "redirect_uuid";
-    private String database_user_data_table_field_yggdrasil_service = "yggdrasil_service";
-    private String database_user_data_table_field_whitelist = "whitelist";
+    private final String database_user_data_table_field_online_uuid = "online_uuid";
+    private final String database_user_data_table_field_current_name = "current_name";
+    private final String database_user_data_table_field_redirect_uuid = "redirect_uuid";
+    private final String database_user_data_table_field_yggdrasil_service = "yggdrasil_service";
+    private final String database_user_data_table_field_whitelist = "whitelist";
     private String database_cache_whitelist_table_name = "{prefix}{_}cache_whitelist";
-    private String database_cache_whitelist_table_field_sign = "sign";
+    private final String database_cache_whitelist_table_field_sign = "sign";
     private String database_skin_restorer_table_name = "{prefix}{_}skin_restorer";
-    private String database_skin_restorer_table_field_online_uuid = "online_uuid";
-    private String database_skin_restorer_table_field_current_skin_url = "current_skin_url";
-    private String database_skin_restorer_table_field_restorer_data = "restorer_data";
+    private final String database_skin_restorer_table_field_online_uuid = "online_uuid";
+    private final String database_skin_restorer_table_field_current_skin_url = "current_skin_url";
+    private final String database_skin_restorer_table_field_restorer_data = "restorer_data";
     private String database_backend_url_h2 = "jdbc:h2:{file_path};TRACE_LEVEL_FILE=0;TRACE_LEVEL_SYSTEM_OUT=0";
     private String database_backend_url_mysql = "jdbc:mysql://{ip}:{port}/{database}?autoReconnect=true&useUnicode=true&amp&characterEncoding=UTF-8&useSSL=false";
-    private boolean verify_yggdrasil_ignore_path = false;
-    private boolean verify_username_allow_duplicate_name = false;
     private boolean logger_debug = false;
 
 
@@ -48,6 +46,7 @@ public class AdvancedSetting {
         properties.load(stream);
         for (Field field : this.getClass().getDeclaredFields()) {
             if (Modifier.isTransient(field.getModifiers())) continue;
+            if (Modifier.isFinal(field.getModifiers())) continue;
             String path = field.getName();
             if (field.getType() == String.class) {
                 field.set(this, properties.getProperty(path, (String) field.get(this)));
