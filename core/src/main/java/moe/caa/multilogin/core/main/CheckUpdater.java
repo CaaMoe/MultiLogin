304036496628dbc4e502ce95be5c34c10a7f73ab
CaaMoe <Aeva_D@hotmail.com>
2021-10-23 18:12:15 +0800

    修复检查更新程序

--- core/src/main/java/moe/caa/multilogin/core/main/CheckUpdater.java
+++ core/src/main/java/moe/caa/multilogin/core/main/CheckUpdater.java
@@ -10,44 +10,44 @@ import moe.caa.multilogin.core.util.ValueUtil;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Locale;
 
 /**
  * 检查更新程序
  */
 public class CheckUpdater {
-    private final URL SOURCE;
+    private final URL source;
     private final MultiCore core;
-    public String latestVersion = null;
-    public boolean haveUpdate = false;
+    public Version latestVersion = null;
 
     public CheckUpdater(MultiCore core) throws MalformedURLException {
         this.core = core;
-        SOURCE = new URL("https://api.github.com/repos/CaaMoe/MultiLogin/contents/gradle.properties?ref=master");
+        source = new URL("https://api.github.com/repos/CaaMoe/MultiLogin/contents/gradle.properties?ref=master");
     }
-
 
     /**
      * 检查更新
      */
     private void check0() throws Exception {
-        // JsonObject json = JsonParser.parseString(HttpUtil.httpGet(SOURCE, 10000, 3)).getAsJsonObject();
-        JsonObject json = JsonParser.parseString(HttpUtil.httpGet(SOURCE, 10000, 3)).getAsJsonObject();
+        JsonObject json = JsonParser.parseString(HttpUtil.httpGet(source, 10000, 3)).getAsJsonObject();
         String sor = json.get("content").getAsString();
         sor = sor.substring(0, sor.length() - 1);
-        String s = new String(ValueUtil.getDECODER().decode(sor), StandardCharsets.UTF_8);
-        latestVersion = s.split("=")[1].trim().toLowerCase(Locale.ROOT);
+        String s = new String(ValueUtil.getDECODER().decode(sor), StandardCharsets.UTF_8).split("=")[1].trim();
+        latestVersion = Version.fromString(s);
     }
 
     public void check() {
+        Version pluginVersion = null;
         try {
             check0();
         } catch (Exception e) {
             MultiLogger.getLogger().log(LoggerLevel.DEBUG, "", e);
         }
-        String pluginVersion = core.getPlugin().getPluginVersion().trim().toLowerCase(Locale.ROOT);
-        haveUpdate = !pluginVersion.equalsIgnoreCase(latestVersion);
-        if (haveUpdate) {
+        try {
+            pluginVersion = Version.fromString(core.getPlugin().getPluginVersion().trim());
+        } catch (Exception ignore){}
+        if(latestVersion == null) return;
+
+        if (pluginVersion == null || pluginVersion.shouldUpdate(latestVersion)) {
             MultiLogger.getLogger().log(LoggerLevel.INFO, "插件的最新版本为 " + latestVersion + "， 请注意及时更新");
         }
     }
---
+++ core/src/main/java/moe/caa/multilogin/core/main/Version.java
@@ -0,0 +1,81 @@
+package moe.caa.multilogin.core.main;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Locale;
+import java.util.Objects;
+
+/**
+ * 版本号解析器
+ */
+@AllArgsConstructor
+public class Version {
+    private final int majorVersion;
+    private final int minorVersion;
+    private final Type greekAlphabet;
+    private final int phaseVersion;
+
+    public static Version fromString(String input){
+        int majorVersion;
+        int minorVersion;
+        Type greekAlphabet;
+        int phaseVersion;
+        String[] args = input.split("\\.");
+        if(args.length < 2) throw new IllegalArgumentException(input);
+        majorVersion = Integer.parseInt(args[0]);
+        if(!args[1].contains("-")) {
+            minorVersion = Integer.parseInt(args[1]);
+            return new Version(majorVersion, minorVersion, Type.RELEASE, -1);
+        }
+        phaseVersion = Integer.parseInt(args[2]);
+        args = args[1].split("-");
+        minorVersion = Integer.parseInt(args[0]);
+        greekAlphabet = Type.valueOf(args[1].toUpperCase(Locale.ROOT));
+        return new Version(majorVersion, minorVersion, greekAlphabet, phaseVersion);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Version version = (Version) o;
+        return majorVersion == version.majorVersion && minorVersion == version.minorVersion && phaseVersion == version.phaseVersion && greekAlphabet == version.greekAlphabet;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(majorVersion, minorVersion, greekAlphabet, phaseVersion);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(majorVersion).append('.').append(minorVersion);
+        if(greekAlphabet != Type.RELEASE){
+            sb.append('-').append(greekAlphabet.name()).append('.').append(phaseVersion);
+        }
+        return sb.toString();
+    }
+
+    public boolean shouldUpdate(Version version){
+        if(version.majorVersion > majorVersion) return true;
+        if(version.majorVersion < majorVersion) return false;
+        if(version.minorVersion > minorVersion) return true;
+        if(version.minorVersion < minorVersion) return false;
+        if(version.greekAlphabet.level > greekAlphabet.level) return true;
+        if(version.greekAlphabet.level < greekAlphabet.level) return false;
+        return version.phaseVersion > phaseVersion;
+    }
+
+    private enum Type {
+        ALPHA(1),
+        BETA(2),
+        RC(3),
+        RELEASE(4);
+
+        private final int level;
+        Type(int level){
+            this.level = level;
+        }
+    }
+}
