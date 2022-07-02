2e5780f4ecb442b948658844dc974cdf0c25f5d5
CatMoe <catmoe_meow@protonmail.com>
2022-07-02 20:57:47 +0800

    :sparkles: Refator: Refact language handler

--- core/src/main/java/moe/caa/multilogin/core/language/LanguageHandler.java
+++ core/src/main/java/moe/caa/multilogin/core/language/LanguageHandler.java
@@ -5,6 +5,7 @@ import moe.caa.multilogin.api.logger.LoggerProvider;
 import moe.caa.multilogin.api.util.Pair;
 import moe.caa.multilogin.api.util.ValueUtil;
 import moe.caa.multilogin.core.main.MultiCore;
+import org.h2.util.IOUtils;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -16,8 +17,7 @@ import java.util.Properties;
  */
 public class LanguageHandler implements LanguageAPI {
     private final MultiCore core;
-    private Properties inside;
-    private Properties outside;
+    private Properties language;
 
     public LanguageHandler(MultiCore core) {
         this.core = core;
@@ -27,36 +27,32 @@ public class LanguageHandler implements LanguageAPI {
      * 初始化这个可读文本处理程序
      */
     public void init() throws IOException {
-        inside = new Properties();
-        try (InputStream is = Objects.requireNonNull(getClass().getResourceAsStream("/message.properties"))) {
-            inside.load(new InputStreamReader(is, StandardCharsets.UTF_8));
+        language = new Properties();
+        if (!new File(core.getPlugin().getDataFolder(), "message.properties").exists()) {
+
+            LoggerProvider.getLogger().info("Loading default messages.");
+            InputStream inputStream = getClass().getResourceAsStream("/message.properties");
+            language.load(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+
+            LoggerProvider.getLogger().info("Extracting message.properties.");
+            File languageFile = new File(core.getPlugin().getDataFolder(), "message.properties");
+            languageFile.createNewFile();
+            OutputStream outputStream = new FileOutputStream(languageFile);
+            IOUtils.copy(inputStream, outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } else {
+            LoggerProvider.getLogger().info("Loading custom messages.");
+            InputStream inputStream = new FileInputStream(new File(core.getPlugin().getDataFolder(), "message.properties"));
+            language.load(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+
         }
-        reloadOutside();
     }
 
     /**
      * 重新加载外置语言仓库
      */
-    public void reloadOutside() {
-        File outsideFile = new File(core.getPlugin().getDataFolder(), "message.properties");
-        if (outsideFile.exists()) {
-            outside = new Properties();
-            try {
-                outside.load(new InputStreamReader(new FileInputStream(outsideFile), StandardCharsets.UTF_8));
-                LoggerProvider.getLogger().info("Use outside language properties.");
-            } catch (IOException e) {
-                LoggerProvider.getLogger().error(String.format("Unable to load outside message file. (%s)", outsideFile.getAbsolutePath()), e);
-            }
-        } else {
-            outside = null;
-        }
-    }
-
     public final String getMessage(String node, Pair<?, ?>... pairs) {
-        if (outside != null && outside.containsKey(node)) {
-            return ValueUtil.transPapi(outside.getProperty(node), pairs);
-        } else {
-            return ValueUtil.transPapi(inside.getProperty(node), pairs);
-        }
+        return ValueUtil.transPapi(language.getProperty(node), pairs);
     }
 }
