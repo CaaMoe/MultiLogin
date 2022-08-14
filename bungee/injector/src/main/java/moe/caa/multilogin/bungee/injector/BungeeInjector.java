ebd8927bba18dcbce98833c663ca3017e0412896
ksqeib <ks@kasukusakura.com>
2022-08-15 00:02:04 +0800

    BungeeInjector重写

--- bungee/injector/src/main/java/moe/caa/multilogin/bungee/injector/BungeeInjector.java
+++ bungee/injector/src/main/java/moe/caa/multilogin/bungee/injector/BungeeInjector.java
@@ -23,13 +23,22 @@ public class BungeeInjector implements Injector {
         redirectIn(Protocol.LOGIN, EncryptionResponse.class, MultiEncryptionResponse::new);
     }
 
-    private synchronized <T> void redirectIn(Protocol protocol, Class<T> originalClass, Supplier<? extends T> redirectSupplier) throws NoSuchFieldException, ClassNotFoundException, IllegalAccessException {
+
+    /**
+     * 重定向输出
+     *
+     * @param stage            进行中的阶段
+     * @param originalClass    原始类
+     * @param redirectSupplier 重定向的Supplier
+     * @param <T>              原始类的类型
+     */
+    private <T> void redirectIn(Protocol stage, Class<T> originalClass, Supplier<? extends T> redirectSupplier) throws NoSuchFieldException, ClassNotFoundException, IllegalAccessException {
         Class<?> c$protocolData = Class.forName("net.md_5.bungee.protocol.Protocol$ProtocolData");
         Field f$packetConstructors = c$protocolData.getDeclaredField("packetConstructors");// Supplier<? extends DefinedPacket>[]
         accessSet(f$packetConstructors);
 
         // 遍历所有版本协议
-        for (Object data : getProtocolDataList(protocol, getServerSideField())) {//ProtocolData
+        for (Object data : getProtocolDataList(stage, true)) {//ProtocolData
             // 获取其入口包构造数据
             Supplier<?>[] definedPackets = (Supplier<?>[]) (f$packetConstructors.get(data)); // ? extends DefinedPacket
 //            寻找该类
@@ -43,7 +52,15 @@ public class BungeeInjector implements Injector {
     }
 
 
-    private synchronized <T, F> void redirectOut(Protocol protocol, Class<T> originalClass, Class<F> redirectClass) throws NoSuchFieldException, ClassNotFoundException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+    /**
+     * 重定向输出
+     *
+     * @param stage         进行中的阶段
+     * @param originalClass 原始类
+     * @param redirectClass 重定向的类
+     * @param <T>           原始类的类型
+     */
+    private <T> void redirectOut(Protocol stage, Class<T> originalClass, Class<? extends T> redirectClass) throws NoSuchFieldException, ClassNotFoundException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Class<?> c$protocolData = Class.forName("net.md_5.bungee.protocol.Protocol$ProtocolData");
         Field f$packetMap = c$protocolData.getDeclaredField("packetMap");// Supplier<? extends DefinedPacket>[]
         accessSet(f$packetMap);
@@ -51,32 +68,31 @@ public class BungeeInjector implements Injector {
         Method m$put = TObjectIntMap.class.getDeclaredMethod("put", Object.class, int.class);
 
         // 遍历所有版本协议
-        for (Object data : getProtocolDataList(protocol, getClientSideField())) {//ProtocolData
+        for (Object data : getProtocolDataList(stage, false)) {//ProtocolData
             TObjectIntMap<?> packetMap = (TObjectIntMap<?>) f$packetMap.get(data);
             if (!packetMap.containsKey(originalClass)) continue;
             m$put.invoke(packetMap, redirectClass, packetMap.get(originalClass));
         }
     }
 
-    private void accessSet(Field... fields) {
-        Arrays.stream(fields).forEach(field -> field.setAccessible(true));
-    }
-
-    private Object[] getProtocolDataList(Protocol protocol, Field sideField) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+    /**
+     * 获取不同方向下的不同阶段的全部协议
+     * @param stage    进行中的阶段
+     * @param toServer 是否是发给服务端方向
+     * @return 全部版本的协议
+     */
+    private Object[] getProtocolDataList(Protocol stage, boolean toServer) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
         Class<?> c$directionData = Class.forName("net.md_5.bungee.protocol.Protocol$DirectionData");
         Field f$protocols = c$directionData.getDeclaredField("protocols");// TIntObjectMap<ProtocolData>
+        Field sideField = toServer ? Protocol.class.getDeclaredField("TO_SERVER") : Protocol.class.getDeclaredField("TO_CLIENT");
         accessSet(f$protocols, sideField);
-        Object directionData = sideField.get(protocol);
+        Object directionData = sideField.get(stage);
         TIntObjectMap<?> protocols = (TIntObjectMap<?>) f$protocols.get(directionData); // ? is ProtocolData
         return protocols.values();
     }
 
-    private Field getServerSideField() throws NoSuchFieldException {
-        return Protocol.class.getDeclaredField("TO_SERVER");// DirectionData
-    }
-
-    private Field getClientSideField() throws NoSuchFieldException {
-        return Protocol.class.getDeclaredField("TO_CLIENT");// DirectionData
+    private void accessSet(Field... fields) {
+        Arrays.stream(fields).forEach(field -> field.setAccessible(true));
     }
 
 }
