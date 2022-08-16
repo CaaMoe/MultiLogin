c7f36d5f06046764b24c6157cd598921e9f8f677
CaaMoe <aeva_d@hotmail.com>
2022-08-16 12:38:57 +0800

    Bukkit 1_19_r1 登录签名移除部分完成

--- bukkit/injector/v1_19_R1/src/main/java/moe/caa/multilogin/bukkit/injector/nms/v1_19_r1/NMSInjector.java
+++ bukkit/injector/v1_19_R1/src/main/java/moe/caa/multilogin/bukkit/injector/nms/v1_19_r1/NMSInjector.java
@@ -4,13 +4,24 @@ import moe.caa.multilogin.api.injector.Injector;
 import moe.caa.multilogin.api.main.MultiCoreAPI;
 import moe.caa.multilogin.api.util.ReflectUtil;
 import moe.caa.multilogin.bukkit.injector.nms.v1_19_r1.handler.MultiPacketLoginInEncryptionBeginHandler;
+import moe.caa.multilogin.bukkit.injector.nms.v1_19_r1.proxy.SignatureValidatorInvocationHandler;
 import moe.caa.multilogin.bukkit.injector.nms.v1_19_r1.redirect.MultiPacketLoginInEncryptionBegin;
+import moe.caa.multilogin.bukkit.main.MultiLoginBukkit;
 import net.minecraft.network.EnumProtocol;
 import net.minecraft.network.PacketDataSerializer;
 import net.minecraft.network.protocol.EnumProtocolDirection;
 import net.minecraft.network.protocol.Packet;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.Services;
+import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.util.SignatureValidator;
+import org.bukkit.craftbukkit.v1_19_R1.CraftServer;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Proxy;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -26,6 +37,32 @@ public class NMSInjector implements Injector {
         if (!redirectInput(EnumProtocol.d, EnumProtocolDirection.a, 0x01, var0 -> new MultiPacketLoginInEncryptionBegin(var0, api))) {
             throw new RuntimeException("0x01 -> new MultiPacketLoginInEncryptionBegin");
         }
+
+        DedicatedServer server = ((CraftServer) ((MultiLoginBukkit) api.getPlugin()).getServer()).getServer();
+
+        Field servicesField = ReflectUtil.handleAccessible(ReflectUtil.findNoStaticField(MinecraftServer.class, Services.class));
+        Object serviceObj  = servicesField.get(server);
+
+        LinkedHashMap<Field, Object> fieldObjectMap = new LinkedHashMap<>();
+        for (Field declaredField  : Services.class.getDeclaredFields()) {
+            if (Modifier.isStatic(declaredField.getModifiers())) {
+                continue;
+            }
+            Object value = ReflectUtil.handleAccessible(declaredField).get(serviceObj);
+            if (value.getClass().getName().contains("SignatureValidator")) {
+                value = Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
+                        new Class[]{SignatureValidator.class}, new SignatureValidatorInvocationHandler(value));
+            }
+            fieldObjectMap.put(declaredField, value);
+        }
+
+        final Constructor<?> declaredConstructor = serviceObj.getClass().getDeclaredConstructor(
+                fieldObjectMap.keySet().stream().map(Field::getType).toArray(Class[]::new)
+        );
+
+        final Object o = declaredConstructor.newInstance(fieldObjectMap.values().toArray());
+
+        servicesField.set(server, o);
     }
 
     /**
