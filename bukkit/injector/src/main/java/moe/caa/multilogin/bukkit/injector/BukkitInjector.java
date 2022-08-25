b8de99f635f5b551ea0db87b6099f6fa0b11c37d
CaaMoe <aeva_d@hotmail.com>
2022-08-25 18:24:30 +0800

    remove BukkitInjector

--- bukkit/injector/src/main/java/moe/caa/multilogin/bukkit/injector/BukkitInjector.java
+++ bukkit/injector/src/main/java/moe/caa/multilogin/bukkit/injector/BukkitInjector.java
@@ -1,7 +1,6 @@
 package moe.caa.multilogin.bukkit.injector;
 
 import moe.caa.multilogin.api.injector.Injector;
-import moe.caa.multilogin.api.logger.LoggerProvider;
 import moe.caa.multilogin.api.main.MultiCoreAPI;
 import moe.caa.multilogin.bukkit.main.MultiLoginBukkit;
 
@@ -21,15 +20,7 @@ public class BukkitInjector implements Injector {
             Injector injector = (Injector) Class.forName(handlerEnum.getNhc()).getConstructor().newInstance();
             injector.inject(api);
         } catch (Throwable t0) {
-            LoggerProvider.getLogger().error("The new injector cannot be used.", t0);
-            try {
-                String generalNHC = "moe.caa.multilogin.bukkit.injector.nms.old.NMSInjector";
-                Injector injector = (Injector) Class.forName(generalNHC).getConstructor().newInstance();
-                injector.inject(api);
-                LoggerProvider.getLogger().warn("With older injectors, there may be some problems.");
-            } catch (Throwable t1) {
-                throw new RuntimeException("Servers with Bukkit version " + nmsVersion + " are not supported.", t1);
-            }
+            throw new RuntimeException("Servers with Bukkit version " + nmsVersion + " are not supported.", t0);
         }
     }
 }
