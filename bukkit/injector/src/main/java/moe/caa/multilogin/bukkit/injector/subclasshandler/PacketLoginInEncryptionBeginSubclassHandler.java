333fe0dbad42368560795d3a9fb2636d83ac30e3
CaaMoe <aeva_d@hotmail.com>
2022-09-07 11:03:30 +0800

    Update PacketLoginInEncryptionBeginSubclassHandler

--- bukkit/injector/src/main/java/moe/caa/multilogin/bukkit/injector/subclasshandler/PacketLoginInEncryptionBeginSubclassHandler.java
+++ bukkit/injector/src/main/java/moe/caa/multilogin/bukkit/injector/subclasshandler/PacketLoginInEncryptionBeginSubclassHandler.java
@@ -7,20 +7,14 @@ import lombok.SneakyThrows;
 import moe.caa.multilogin.bukkit.injector.BukkitInjector;
 import moe.caa.multilogin.bukkit.main.MultiLoginBukkit;
 import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.asm.Advice;
-import net.bytebuddy.description.modifier.Visibility;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.implementation.bind.annotation.AllArguments;
-import net.bytebuddy.implementation.bind.annotation.SuperCall;
+import net.bytebuddy.implementation.bind.annotation.Morph;
 import net.bytebuddy.matcher.ElementMatchers;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.reflect.Field;
 import java.util.Locale;
 import java.util.UUID;
-import java.util.concurrent.Callable;
 
 /**
  * 代理 PacketLoginInEncryptionBegin 这个数据包
@@ -29,32 +23,25 @@ public class PacketLoginInEncryptionBeginSubclassHandler {
     private final BukkitInjector injector;
     @Getter
     private Class<?> proxyLoginInEncryptionBeginClass;
-    private MethodHandle multilogin_original_handlerFieldSetter;
-    private MethodHandle multilogin_original_handlerFieldGetter;
 
     public PacketLoginInEncryptionBeginSubclassHandler(BukkitInjector injector) {
         this.injector = injector;
     }
 
-    public void init() throws NoSuchFieldException, IllegalAccessException {
+    public void init() {
         String name = "moe.caa.multilogin.bukkit.injector.PacketLoginInEncryptionBegin$" + UUID.randomUUID().toString().substring(0, 6).toUpperCase(Locale.ROOT);
-        String original_handler = "multilgoin_original_handler";
         proxyLoginInEncryptionBeginClass = new ByteBuddy()
                 .subclass(injector.getPacketLoginInEncryptionBeginClass())
                 .name(name)
-                .defineField(original_handler, injector.getPacketLoginInEncryptionBeginClass(), Visibility.PUBLIC)
                 .method(ElementMatchers.takesArguments(1)
                         .and(ElementMatchers.takesArguments(injector.getPacketLoginInListenerClass())))
-                .intercept(MethodDelegation.to(new HandleMethodInterceptor()))
+                .intercept(MethodDelegation.withDefaultConfiguration()
+                        .withBinders(Morph.Binder.install(Morpher.class))
+                        .to(new ModifyLoginListenerArg()))
                 .make()
                 .load(((MultiLoginBukkit) injector.getApi().getPlugin()).getMlPluginLoader()
                         .getPluginClassLoader().self(), ClassLoadingStrategy.Default.WRAPPER)
                 .getLoaded();
-
-        MethodHandles.Lookup lookup = MethodHandles.lookup();
-        Field field = proxyLoginInEncryptionBeginClass.getField(original_handler);
-        multilogin_original_handlerFieldSetter = lookup.unreflectSetter(field);
-        multilogin_original_handlerFieldGetter = lookup.unreflectGetter(field);
     }
 
     @SneakyThrows
@@ -62,15 +49,16 @@ public class PacketLoginInEncryptionBeginSubclassHandler {
         return proxyLoginInEncryptionBeginClass.getDeclaredConstructors()[0].newInstance(args);
     }
 
+    public interface Morpher {
+        Object invoke(Object[] args);
+    }
+
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
-    public class HandleMethodInterceptor {
+    public class ModifyLoginListenerArg {
 
         public Object intercept(
-                @SuperCall Callable<Object> supercall,
-                @AllArguments Object[] args) throws Throwable {
-            @Advice.AllArguments
-                    args[0] = null;
-            return supercall.call();
+                @Morph Morpher morpher, @AllArguments Object[] args) throws Throwable {
+            return morpher.invoke(new Object[]{injector.getLoginListenerSubclassHandler().newFakeProxyLoginListener(args[0])});
         }
     }
 }
