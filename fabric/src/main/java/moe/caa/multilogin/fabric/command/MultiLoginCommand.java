2ba1f1152c897e6a6d5264c4101c70535481133f
CaaMoe <aeva_d@hotmail.com>
2022-09-14 10:33:01 +0800

    指令修复

--- fabric/src/main/java/moe/caa/multilogin/fabric/command/MultiLoginCommand.java
+++ fabric/src/main/java/moe/caa/multilogin/fabric/command/MultiLoginCommand.java
@@ -1,8 +1,11 @@
 package moe.caa.multilogin.fabric.command;
 
+import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import moe.caa.multilogin.fabric.impl.FabricSender;
@@ -13,7 +16,7 @@ import net.minecraft.server.command.ServerCommandSource;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-public class MultiLoginCommand {
+public class MultiLoginCommand implements SuggestionProvider<ServerCommandSource>, Command<ServerCommandSource> {
     private final MultiLoginFabric plugin;
 
     public MultiLoginCommand(MultiLoginFabric plugin) {
@@ -24,26 +27,28 @@ public class MultiLoginCommand {
         dispatcher.register(
                 CommandManager.literal("multilogin")
                         .then(CommandManager.argument("args", StringArgumentType.greedyString())
-                                .suggests(this::suggests)
-                                .executes(this::execute)
+                                .suggests(this)
+                                .executes(this)
                         )
         );
     }
 
-    private CompletableFuture<Suggestions> suggests(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) {
+    @Override
+    public CompletableFuture<Suggestions> getSuggestions(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) throws CommandSyntaxException {
         FabricSender sender = new FabricSender(context.getSource());
         String s = builder.getRemainingLowerCase();
         String[] args = s.split("\\s+");
         String[] ns = new String[args.length + 1];
         System.arraycopy(args, 0, ns, 1, args.length);
         ns[0] = "multilogin";
-        // todo 有问题的
+        builder = builder.createOffset(builder.getInput().lastIndexOf(' ') + 1);
         List<String> list = plugin.getApi().getCommandHandler().tabComplete(sender, ns);
         list.forEach(builder::suggest);
         return builder.buildFuture();
     }
 
-    private int execute(CommandContext<ServerCommandSource> context) {
+    @Override
+    public int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
         FabricSender sender = new FabricSender(context.getSource());
         String[] args = StringArgumentType.getString(context, "args").split("\\s+");
         String[] ns = new String[args.length + 1];
