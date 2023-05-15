7046d25cbbde1ccb95d7757bf4ee9ea7cbb35734
CaaMoe <aeva_d@hotmail.com>
2023-05-15 10:50:19 +0800

    (#98) mapper

--- core/src/main/java/moe/caa/multilogin/core/auth/service/floodgate/FloodgateAuthenticationService.java
+++ core/src/main/java/moe/caa/multilogin/core/auth/service/floodgate/FloodgateAuthenticationService.java
@@ -35,7 +35,7 @@ public class FloodgateAuthenticationService implements HandshakeHandler {
         BedrockData data = handshakeData.getBedrockData();
         String xuid = data.getXuid();
         UUID uuid = ValueUtil.xuidToUUID(xuid);
-        GameProfile profile = new UnmodifiableGameProfile(uuid, handshakeData.getBedrockData().getUsername(), new HashMap<>());
+        GameProfile profile = new UnmodifiableGameProfile(uuid, initBedrockUsername(handshakeData.getBedrockData().getUsername()), new HashMap<>());
         FloodgateServiceConfig service = multiCore.getPluginConfig().getFloodgateAuthenticationService();
         if (service == null) {
             handshakeData.setDisconnectReason(multiCore.getLanguageHandler().getMessage("auth_floodgate_service_notfound"));
@@ -53,5 +53,18 @@ public class FloodgateAuthenticationService implements HandshakeHandler {
         } else {
             handshakeData.setDisconnectReason(loginAuthResult.getKickMessage());
         }
+    }
+
+    private String initBedrockUsername(String bedrockUsername) {
+        char[] charArray = bedrockUsername.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : charArray) {
+            stringBuilder.append(isNameAllowedCharacter(c) ? c : '_');
+        }
+        return stringBuilder.toString();
+    }
+
+    private boolean isNameAllowedCharacter(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || c == '_';
     }
 }
