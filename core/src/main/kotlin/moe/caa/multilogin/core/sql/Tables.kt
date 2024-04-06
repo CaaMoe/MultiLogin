df9dda16f4d7be46f4c4f2e45f2431215d7a9b2f
秋雨落 <i@rain.cx>
2024-04-06 18:37:42 +0800

    database: remove useless user data table.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- core/src/main/kotlin/moe/caa/multilogin/core/sql/Tables.kt
+++ core/src/main/kotlin/moe/caa/multilogin/core/sql/Tables.kt
@@ -3,18 +3,15 @@ package moe.caa.multilogin.core.sql
 import org.jetbrains.exposed.dao.id.IntIdTable
 
 object InGameProfileV3Table : IntIdTable(name = "multilogin_in_game_profile_v3") {
-    val user = reference("user_id", UserDataV3Table)
-    val inGameUUID = uuid("in_game_uuid").uniqueIndex()
+
+    /**
+     * null for any service. (for caching whitelist).
+     */
+    val serviceId = integer("service_id").nullable()
+
+    val inGameUUID = uuid("profile_uuid").uniqueIndex()
     val currentUsernameLowerCase = varchar("current_username_lower_case", 64).uniqueIndex()
     val currentUsernameOriginal = varchar("current_username_original", 64).uniqueIndex()
-}
-
-object UserDataV3Table : IntIdTable(name = "multilogin_user_data_v3") {
-    val profile = reference("profile_id", InGameProfileV3Table).nullable()
-
-    val onlineUUID = uuid("online_uuid")
-    val serviceId = integer("service_id")
-    val onlineName = varchar("online_name", 64).nullable()
 
     /**
      * 0: No whitelist
@@ -23,7 +20,5 @@ object UserDataV3Table : IntIdTable(name = "multilogin_user_data_v3") {
      */
     val whitelist = integer("whitelist").check("check_whitelist_range") { it.between(0, 2) }
 
-    init {
-        uniqueIndex(serviceId, onlineUUID)
-    }
+    val redirectTo = reference("redirect_to_profile_id", InGameProfileV3Table).nullable()
 }
